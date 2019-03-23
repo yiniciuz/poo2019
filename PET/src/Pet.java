984fc76186543fc1859e85f18166130caaa3a499
Vinícius Melo <vmcmxcviii@gmail.com>
2019-03-23 12:48:24 -0300

    up

---
+++ PET/src/Pet.java
@@ -0,0 +1,26 @@
+public class Pet {
+    String nome;
+    int energia;
+    int saciedade;
+    int limpeza;
+    /*__int idade;
+    int diamantes;*/
+    int energiaMax;
+    int saciedadeMax;
+    int limpezaMax;
+
+    public Pet(String name, int energia,int saciedade,int limpeza){
+        this.nome = name;
+        this.energia = energia;
+        this.saciedade = saciedade;
+        this.limpeza = limpeza;
+        this.energiaMax = energiaMax;
+        this.saciedadeMax = saciedadeMax;
+        this.limpezaMax = limpezaMax;
+    }
+
+
+
+
+
+}
