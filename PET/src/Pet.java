fd9a4021c2bf06ecdd2503fab70845c1b69a5ad1
Vinícius Melo <vmcmxcviii@gmail.com>
2019-03-23 13:53:02 -0300

    up

--- PET/src/Pet.java
+++ PET/src/Pet.java
@@ -1,3 +1,7 @@
+import com.sun.source.doctree.SystemPropertyTree;
+
+import java.util.Scanner;
+
 public class Pet {
     String nome;
     int energia;
@@ -19,8 +23,24 @@ public class Pet {
         this.limpezaMax = limpezaMax;
     }
 
+    public String toString() {
+        return "[" + this.name + "] " +
+                "E: " + this.energia + "/" + this.energiaMax + ", " +
+                "S: " + this.saciedade + "/" + this.saciedadeMax + ", " +
+                "L: " + this.limpeza + "/" + this.limpezaMax;
+    }
+}
 
+class Controller{
+    public static void main(Sting[] args) {
+        Scanner scan = new Scanner(System.in); /*__ cria uma variavel scan do tipo
+                                                Scanner que o parametro é System in, uma entrada do cliente*/
+        Pet pet = new Pet(" ",0, 0, 0);
+        System.out.println("init, show, end");
+        while(true){
+            String[] ui; /*__ O '[]' significa que se trata de uma vetor (lista) do tipo string */
 
-
+        }
+    }
 
 }
