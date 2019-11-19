e2bd21270f883d4a2993ccd30dc0ed214c2e49fd
Vinícius Melo <vmcmxcviii@gmail.com>
2019-11-19 16:09:19 -0300

    up

--- MiAu/src/TesteMain.java
+++
@@ -1,8 +0,0 @@
-public class TesteMain {
-    public static void main (String [] args) {
-        Passeio passeio = new Passeio("24 out", 17f, 2f);
-        System.out.println(passeio);
-        passeio.pagamento();
-        System.out.println(passeio.pagamento());
-    }
-}
